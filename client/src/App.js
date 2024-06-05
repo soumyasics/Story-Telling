c081ed2e6cfedcaf746b106ecfe53bd656d5264c
Ajeenasics <ajeena.sics@gmail.com>
2024-06-05 10:40:14 +0530

    story telling started

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AdminLogin from "./Components/Admin/AdminLogin";
+import ViewerLogin from "./Components/Viewer/ViewerLogin";
+import ViewerRegister from "./Components/Viewer/ViewerRegister";
+import EditorLogin from "./Components/Editor/EditorLogin";
+import EditorRegister from "./Components/Editor/EditorRegister";
+import Landingpage from "./Components/Pages/Landingpage";
+import Header from "./Components/Pages/Header";
+import Footer from "./Components/Pages/Footer";
+import Viewerforgotpassword from "./Components/Viewer/Viewerforgotpassword";
+import Editorforgotpassword from "./Components/Editor/Editorforgotpassword";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/adminlogin" element=<AdminLogin /> />
+        <Route path="/viwerlogin" element={[<Header/>,<ViewerLogin />,<Footer/>]} />
+        <Route path="/editorlogin" element={[<Header/>,<EditorLogin />,<Footer/>]} />
+        <Route path="/viwerregister" element={[<Header/>,<ViewerRegister /> ,<Footer/>]}/>
+        <Route path="/editorregister" element={[<Header/>,<EditorRegister />,<Footer/>]} />
+        <Route path='/landingpage' element=<Landingpage/> />
+        <Route path="/viewerforgotpassword" element={[<Header/>,<Viewerforgotpassword /> ,<Footer/>]}/>
+        <Route path="/editorforgotpassword" element={[<Header/>,<Editorforgotpassword /> ,<Footer/>]}/>
+        <Route path="/landingpage" element={[<Header/>,<Landingpage/> ,<Footer/>]}/>
+        <Route path='/header' element=<Header/> />
+        <Route path='/footer' element=<Footer/> />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/BaseAPIs/axiosinstatnce.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+   baseURL: "http://localhost:4025",
+  
+//   baseURL:"http://hybrid.srishticampus.in/story_telling_api/"
+
+  // headers: {
+  //   "Content-Type": "multipart/form-data", // Since you're sending form data
+  //   Authorization: "Bearer YOUR_ACCESS_TOKEN", // Include your authorization token if needed
+  // },
+});
+
+export default axiosInstance;
