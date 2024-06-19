ecf6429213990945049bc2f469326a1b9509e73a
soumyasics <soumya.sics@gmail.com>
2024-06-19 22:47:11 +0530

    o

--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,21 @@ const router=express.Router()
 const Writer=require('./Controller/WriterController')
 const Reader=require('./Controller/readerController')
 
+// common routes
+router.post('/login',Reader.login)
+router.post('/forgotPassword',Writer.forgotPWDsentMail)
+router.post('/reset-password/:id',Writer.resetPassword)
+
+
 //Writer routes
 router.post('/registerWriter',Writer.upload,Writer.registerWriter)
-router.post('/loginWriter',Writer.login)
-router.post('/forgotPasswordWriter',Writer.forgotPWDsentMail)
-router.post('/reset-password/:id',Writer.resetPassword)
+
+router.post('/addPayment/:id',Writer.addPayment)
 
 //reader routes
 router.post('/registerReader',Reader.upload,Reader.registerReader)
 router.post('/loginReader',Reader.login)
+router.post('/upgradeToWriter/:id',Reader.upgradeToWriter)
+
 
 module.exports=router
